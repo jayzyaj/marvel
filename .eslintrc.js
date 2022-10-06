325b26b76946a293e9487c8908cf9b38b22490ac
John Clifford de Vera <johnclifforddevera@Johns-MacBook-Pro.local>
2022-10-06 18:03:24 +0800

    Add tailwind config and daisy UI

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'eslint-config-prettier',
-  ],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx', '*.js', '*.jsx', '*.json'],
-      rules: {
-        'prettier/prettier': [
-          'error',
-          {
-            singleQuote: true,
-            printWidth: 120,
-            trailingComma: 'es5',
-            endOfLine: 'auto',
-          },
-        ],
-      },
-    },
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'eslint-config-prettier', 'plugin:storybook/recommended'],
+  overrides: [{
+    files: ['*.ts', '*.tsx', '*.js', '*.jsx', '*.json'],
+    rules: {
+      'prettier/prettier': ['error', {
+        singleQuote: true,
+        printWidth: 120,
+        trailingComma: 'es5',
+        endOfLine: 'auto'
+      }]
+    }
+  }],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint', 'eslint-plugin-prettier', 'prettier'],
-  rules: {},
+  rules: {}
 };
