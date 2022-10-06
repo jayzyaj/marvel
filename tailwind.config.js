042c26f8e4efe0554fe2e0864901413659b3d45e
John Clifford de Vera <johnclifforddevera@Johns-MacBook-Pro.local>
2022-10-06 18:26:07 +0800

    add types

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {},
   },
   plugins: [require('daisyui')],
